3f09efd54fd7f05f4de8d7afccade4c92d961c2b
michele098 <110670842+michele098@users.noreply.github.com>
2024-04-04 07:58:50 -0300

    Add files via upload

--- manin.js
+++ manin.js
@@ -1,2 +1,22 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+
+for(let i=0;i <botoes.length;i++){
+    botoes [I].onclick = function(){
+
+        for(let j=0;<botoes.length;++)}
+           botoes[j]. classList.remove ("ativo");
+
+
+
+       botoes[i].classList.add("ativo");
+    
+    console.log();
+}
+
+    
+
+
+
+
+
+
